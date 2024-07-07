f29b34a8ae50e2aa24bfa67ce6b4ea32691b783d
Pramod_Acharya <pramodacharya808@gmail.com>
2024-07-07 22:09:39 +0530

    node version change

--- src/config/web-sockets.js
+++ src/config/web-sockets.js
@@ -1,10 +1,10 @@
-import io from 'socket.io-client';
+import io from "socket.io-client";
 
 let STRAPI_ENDPOINT;
-if (process.env.NODE_ENV !== 'production') {
-    STRAPI_ENDPOINT = 'http://localhost:1337';
+if (process.env.NODE_ENV !== "production") {
+  STRAPI_ENDPOINT = "http://localhost:1337";
 } else {
-    STRAPI_ENDPOINT = process.env.REACT_APP_SERVER_URL
+  STRAPI_ENDPOINT = process.env.REACT_APP_SERVER_URL;
 }
 
 export const socket = io(STRAPI_ENDPOINT);
